1b4f14fe93878843cea40928549236fd21068511
castrojuan <68036387+castrojuan@users.noreply.github.com>
2020-08-10 19:46:04 -0300

    Add files via upload

--- uno.js
+++ uno.js
@@ -9,5 +9,66 @@ pedir datos por prompt y mostrar por document.write o console.log
 */
 function mostrar()
 {
-	alert("uno");
+	let nombre;
+	let peso;
+	let sexo;
+	let edad;
+	let contadormujeres=0;
+	let pesomax;
+	let hombremax;
+	let flag=0;
+	let contador=0;
+	let acumtotal=0;
+
+
+
+
+
+	for(let i=0 ; i<2; i++)
+
+{
+		
+			nombre=prompt("ingrese su nombre");
+
+
+	    do{
+	    	peso=parseInt(prompt("ingrese el peso"));
+         }while(peso<0 && peso>400);
+
+        do{
+        	sexo=prompt("ingrese el sexo");
+        }while(sexo!="f" && sexo!="m");
+
+        do{
+        	edad=prompt("ingrese su edad");
+        }while(edad<0 || edad>110);
+
+    
+    
+
+  if(sexo=="f")
+   {
+   	contadormujeres++;
+   }
+
+if(sexo=="m")
+{
+ 	pesomax=peso;
+    hombremax=nombre;
+    flag=1;
+}
+if(pesomax<pesomax)
+{
+	pesomax=peso;
+	hombremax=nombre;
+}
+
+}
+
+
+document.write("la cantidad de mujeres es " + contadormujeres +"<br>");
+document.write("la edad promedio es " + acumtotal/contador.toFixed(2) + "<br>");
+document.write("el hombre mas pesado es " + hombremax + " y pesa " + pesomax + "<br>");
+
+
 }
