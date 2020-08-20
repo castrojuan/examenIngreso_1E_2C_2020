9f0bc6e66c7d0c26f6b24b7740a620d1c366a0bd
castrojuan <cjuanmartin780@gmail.com>
2020-08-20 00:07:00 -0300

    definitivo

--- uno.js
+++ uno.js
@@ -21,15 +21,63 @@ let pesoMaximo;
 let HombreMasPesado;
 let banderaPeso=0;
 
-
 for(let i=0; i<5; i++)
 {
+
   do
   {
     nombre=prompt("ingrese nombre");
-  }while(!(IsNaN(nombre)));
-  console.log(nombre);
+  }while(!(isNaN(nombre)));
+
+
+  do
+  {
+  	peso=parseInt(prompt("ingrese el peso"));
+  }while(peso<1 || peso>300 || isNaN(peso));
+
+  do
+  {
+  	sexo=prompt("ingrese su sexo");
+  }while(sexo!="f" && sexo!="m");
+
+  do
+  {
+  	edad=parseInt(prompt("ingrese la edad"));
+  }while(edad<1 || edad>100);
+
+
+contadorTotal++;
+acumTotal=acumTotal+edad;
+
+if(sexo=="f")
+{
+	contMujeres++;
+}
+
+else
+{
+	if(banderaPeso==0 || pesoMaximo<peso)
+	{
+		pesoMaximo=peso;
+		HombreMasPesado=nombre;
+		banderaPeso=1;
+	}
+
+}
+  
+
 }
 
 
-} 
+
+
+document.write("la cantidad de mujeres es " + contMujeres + "<br>");
+document.write("la edad promedio en total es " + acumTotal/contadorTotal + "<br>");
+
+if(banderaPeso==1)
+{
+	document.write("el hombre mas pesado es " + HombreMasPesado + "<br>");
+}
+
+
+}
